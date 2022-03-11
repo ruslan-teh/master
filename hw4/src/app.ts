b24239deb2d918cbd39a396507fc1ffd213b2009
ruslana380900 <ruslanachernenko94@gmail.com>
2022-03-11 22:25:01 +0200

    database

---
+++ hw4/src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.end();
+});
+
+app.listen(5500, () => {
+    console.log('Server has started!!!');
+});
